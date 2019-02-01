686bee10ce2b770774621f9276c802197f390dfd
Gregor Žagar <gregor.zagar@gmail.com>
2019-02-01 07:26:03 +0100

    “rewrite”

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,27 +1,22 @@
 # -*- coding: utf-8 -*-
-def fizzbuzz(stevilo):
-    for x in range(1, stevilo):
-        if x % 3 == 0 and x % 5 == 0:
-            print "fizzbuzz"
-        elif x % 5 == 0:
-            print "buzz"
-        elif x % 3 == 0:
-            print "fizz"
-        else:
-            print x
+run = True
 
-
-def vpis_stevila():
+while run == True:
     try:
         stevilo = int(raw_input("Vnesi število med 1 in 100\n"))
         if stevilo <= 100 and stevilo >= 1:
-            fizzbuzz(stevilo)
+            for x in range(1, stevilo + 1):
+                if x % 3 == 0 and x % 5 == 0:
+                    print "fizzBuzz"
+                elif x % 5 == 0:
+                    print "buzz"
+                elif x % 3 == 0:
+                    print "fizz"
+                else:
+                    print x
+            run = False
         else:
             print "Število je premajhno ali preveliko."
-            vpis_stevila()
+
     except ValueError:
         print "Kar si vpisal, ni število..."
-        vpis_stevila()
-
-
-vpis_stevila()
